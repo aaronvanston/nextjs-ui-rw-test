d81594edcca2bae232c5e224fae682b8a39ff639
Aaron Vanston <aaronvanston@users.noreply.github.com>
2021-12-14 09:50:01 +1100

    refactor: set-up apollo call to RW backend

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from '@apollo/client'
 import '../styles/globals.css'
 
+const client = new ApolloClient({
+  uri: 'https://rw-vercel-test.vercel.app/api/graphql',
+  cache: new InMemoryCache(),
+})
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
 
 export default MyApp
